e19496a57d91b9cd150c7a25b86430f54848cc6c
Tobby Lie <tobbylie@gmail.com>
2019-09-07 23:10:14 -0400

    Added more input validation

--- Tobby_Lie_Lab1.py
+++ Tobby_Lie_Lab1.py
@@ -1,7 +1,7 @@
 # Tobby Lie
 # CSCI 4742
 # Lab1
-# Last modified: 9/5/19 @ 11:38PM
+# Last modified: 9/7/19 @ 9:09PM
 
 import ipaddress
 
@@ -28,7 +28,7 @@ def parseIP(cidrNotation):
 
 
 def convertCidrToDec(cidrSubnet):
-    '''returns netmask given an cidr subnet'''
+    '''returns netmask given a cidr subnet'''
     # initiallize netmask to empty string
     netmask = ""
     # to construct netmask, 1's are added to the left most of the address
@@ -72,8 +72,8 @@ def convertCidrToDec(cidrSubnet):
     # return as a tuple containing netmask 1-4
     return netmask1, netmask2, netmask3, netmask4
 
-
 def printIPsInSubnet(ip, netmask):
+    '''Used to print all addresses in host IDs for third and fourth octets depending on subnet length'''
     # if 3rd octet in net mask is less than 255
     # we subtract it from 255
     if int(netmask[2]) < 255:
@@ -81,20 +81,24 @@ def printIPsInSubnet(ip, netmask):
         # based on the value of netmask[2]
         count3 = 255 - int(netmask[2])
         # for all values from 0 to count3 populate addresses from 1-255
+        # populate all host IDs
         for loop3 in range(0, count3):
             for loop4 in range(1, 256):
-                print("{}.{}.{}.{}".format(ip[0], ip[1], loop3, loop4))
+                # had to add in str(int(ip[x])+loop(x+1) into code to make it consistent with module used
+                print("{}.{}.{}.{}".format(ip[0], ip[1], str(int(ip[2])+loop3), str(int(ip[3])+loop4)))
     # else if 4th octet in net mask is less than 255
-    # subtract it from 255
+    # subtract it from 255    elif int(netmask[3]) < 255:
     elif int(netmask[3]) < 255:
         # count4 is the available number of addresses
         # based on the value of netmask[3]
         count4 = 255 - int(netmask[3])
         # for all values from 0 to count 4 populate addresses from 1-255
         for loop4 in range(1, count4):
-            print("{}.{}.{}.{}".format(ip[0], ip[1], ip[2], loop4))
+            # had to add in str(int(ip[x])+loop(x+1) into code to make it consistent with module used
+            print("{}.{}.{}.{}".format(ip[0], ip[1], ip[2], str(int(ip[3])+loop4)))
 
-def validateInput(cidrNotation):
+def validateInput(cidrNotation, input_cidr):
+    '''Used to validate user input to verify that cidrNotation is in correct format'''
     # try-except block 1
     try:
         # if either the '.' or the '/' in cidrNotation is the first character
@@ -108,12 +112,14 @@ def validateInput(cidrNotation):
     ip = addressSplit[0]
     cidrSubnet = addressSplit[1]
     ipSplit = ip.split('.')
+    # try-except block 2
     try:
         # are there 3 dots?
         if cidrNotation.count(".") != 3:
             return False
     except ValueError:
         return False
+    # try-except block 3
     try:
         # are there 4 numbers in between the dots?
         if len(ipSplit) != 4:
@@ -121,12 +127,14 @@ def validateInput(cidrNotation):
     except ValueError:
         return False
     # first number - left most (significant one) can not be 0, but the rest can be 0
+    # try-except block 4
     try:
         if int(ipSplit[0]) < 1 or int(ipSplit[0]) > 255:
             return False
     except ValueError:
         return False
     # numbers 2-4 can be zero but cannot be greater than 255
+    # try-except block 5
     try:
         # loop through all ip's starting at ip 2
         # in ipSplit and make sure
@@ -138,8 +146,17 @@ def validateInput(cidrNotation):
         return False
     # the subnet size must be less than 32
     # and also greater than 0
+    # try-except block 6
     try:
         if int(cidrSubnet) < 0 or int(cidrSubnet) > 32:
+            return False
+    except ValueError:
+        return False
+    # try-except block 7
+    # this block is very important
+    # without it we would get host bits set error
+    try:
+        if cidrNotation != input_cidr:
             return False
     except ValueError:
         return False
@@ -153,32 +170,20 @@ def main():
     cidrNotation = input("Enter your network in CIDR: ")
     # input validation
     # if incorrect input just exit program
-    if not validateInput(cidrNotation):
-        # incorrect input message
-        print("wrong input: input is not in CIDR format")
-        return
-
     # parseString contains (['first number', 'secondn number', 'third number', 'fourth number'], 'subnet')
     parseString = parseIP(cidrNotation)
     #  ip is now set to ['first number', 'secondn number', 'third number', 'fourth number']
     ip = parseString[0]
     # cidrSubnet is not set to 'subnet' from above
     cidrSubnet = parseString[1]
-    # prompt that you are using functions made from scratch
-    print("Using my own functions")
-    # print the entire IP and the subnet specifying the network ID size
-    print("IP:{}.{}.{}.{}\tCIDR:{}".format(ip[0], ip[1], ip[2], ip[3], cidrSubnet))
-
-    # get netmask by converting cidrSubnet into binary bitmask and then converting to decimal
-    netmask = convertCidrToDec(cidrSubnet)
-
-    # print subnet and all IPs in subnet
-    print("subnet: {}.{}.{}.{}".format(netmask[0], netmask[1], netmask[2], netmask[3]))
-    print("IPs in Subnet:")
-    printIPsInSubnet(ip, netmask)
-
     ############################################################################################
     # this section is necessary to prevent "host bits set" error
+    # when given cidrNotation with a subnet length, there must be zero bits
+    # to the right of the kth bit, k being the subnet length
+    # there must be 32-k zero bits for the host addresses
+    # if this is not correct, it is invalid input as there would already be
+    # a bit in the place where there must be host IDs
+
     netmask_2 = ""
     # get bitmask for cidrSubnet
     for x in range(0, int(cidrSubnet)):
@@ -204,24 +209,32 @@ def main():
         elif bit == '0':
             newCidrBit += '0'
 
+    # new variable to hold correct value given the subnet length
+    # will be used against actual input to make sure it is input correctly
+    # and there is enough space for host IDs
     input_CidrBit = ""
 
+    # create first_octet of just bits
     first_octet = ""
     for bit in newCidrBit[:8]:
         first_octet += bit
 
+    # create second_octet of just bits
     second_octet = ""
     for bit in newCidrBit[8:16]:
         second_octet += bit
 
+    # create third octet of just bits
     third_octet = ""
     for bit in newCidrBit[16:24]:
         third_octet += bit
 
+    # create fourth octet of just bits
     fourth_octet = ""
     for bit in newCidrBit[24:32]:
         fourth_octet += bit
 
+    # construct address from bits converted to decimal
     input_CidrBit += str(int(first_octet, base=2))
     input_CidrBit += "."
     input_CidrBit += str(int(second_octet, base=2))
@@ -231,13 +244,32 @@ def main():
     input_CidrBit += str(int(fourth_octet, base=2))
     input_CidrBit += "/"
     input_CidrBit += cidrSubnet
-
     ############################################################################################
+
+    # validate input before using it for any functions
+    # if input invalid, gracefully exit program
+    if not validateInput(cidrNotation, input_CidrBit):
+        # incorrect input message
+        print("wrong input: input is not in CIDR format")
+        return
+
+    # prompt that you are using functions made from scratch
+    print("Using my own functions")
+    # print the entire IP and the subnet specifying the network ID size
+    print("IP:{}.{}.{}.{}\tCIDR:{}".format(ip[0], ip[1], ip[2], ip[3], cidrSubnet))
+
+    # get netmask by converting cidrSubnet into binary bitmask and then converting to decimal
+    netmask = convertCidrToDec(cidrSubnet)
+
+    # print subnet and all IPs in subnet
+    print("subnet: {}.{}.{}.{}".format(netmask[0], netmask[1], netmask[2], netmask[3]))
+    print("IPs in Subnet:")
+    printIPsInSubnet(ip, netmask)
 
     # using the ipaddress module of Python
     print("Using import ipaddress:")
     # get network based on cidrNotation
-    network = ipaddress.ip_network(input_CidrBit)
+    network = ipaddress.ip_network(cidrNotation)
     # print out the IP and subnet
     print("IP:{}".format(network, cidrSubnet))
     print("subnet:{}".format(network.netmask))
